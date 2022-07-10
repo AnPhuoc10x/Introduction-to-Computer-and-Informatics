1bbcf83d0185d585dac9b2984032617615ed2a0c
AnPhuoc10x <105622510+AnPhuoc10x@users.noreply.github.com>
2022-07-10 11:39:19 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function ProductButton1Click () {
+	document.getElementById("ProductInfo1").style.display="block";
+}
+document.querySelector(".ShowmoreButton1").addEventListener('click',ProductButton1Click);
+
+
+function ProductButton2Click () {
+	document.getElementById("ProductInfo2").style.display="block";
+}
+document.querySelector(".ShowmoreButton2").addEventListener('click',ProductButton2Click);
